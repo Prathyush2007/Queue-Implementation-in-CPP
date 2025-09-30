d5f2a5d9c5e27afdf9d5fdbe06963a9f66e7f140
Prathyush2007 <prathyush.nimmagadda.btech2024@sitpune.edu.in>
2025-09-30 22:11:50 +0530

    Create Add, Delete the members in the queue. Check the queue is overflow or empty.cpp

---
+++ Add, Delete the members in the queue. Check the queue is overflow or empty.cpp
@@ -0,0 +1,60 @@
+//Name:Prathyush
+//Prn:24070123064
+//Class:Entc A3
+#include <iostream>
+using namespace std;
+
+#define SIZE 5
+class Queue{
+    int arr[SIZE];
+    int front,rear;
+    public:
+    Queue(){
+        front = -1;
+        rear = -1;
+    }
+    void enqueue(int value){
+        if(rear==SIZE-1){
+            cout << "Queue Overflow!" << endl;
+            return;
+        }
+        if(front == -1)front = 0;
+        arr[++rear] = value;
+        cout << value << " Inserted into queue."<<endl << endl;
+    }
+    void dequeue(){
+        if(front == -1 || front > rear) {
+            cout << "Queue Underflow!" << endl;
+            return;
+        }
+        cout << arr[front] << " Removed from queue." << endl<< endl;
+        front++;
+    }
+    void display(){
+        if(front==-1 || front > rear){
+            cout << "Queue is empty." << endl;
+            return;
+        }
+        cout << "Queue elements:";
+        for(int i=front;i<=rear;i++){
+            cout << arr[i] << " ";
+        }
+        cout << endl<<endl;
+    }
+};
+int main(){
+    Queue q;
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    q.display();
+    q.dequeue();
+    q.display();
+    q.enqueue(40);
+    q.display();
+    q.enqueue(50);
+    q.enqueue(60);
+    q.enqueue(70);
+    q.display();
+     return 0;
+}
